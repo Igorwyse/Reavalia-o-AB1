be517e62d2630b91b1792d75995c9b3cf4426730
Igorwyse <74892130+Igorwyse@users.noreply.github.com>
2020-12-23 15:54:14 -0300

    Add files via upload

---
+++ Questão1.py
@@ -0,0 +1,15 @@
+idade_doador = input("Qual a sua idade? ")
+idade_doador = int(idade_doador)
+
+if idade_doador < 16:
+    print("Impedimento: menor de 16 anos.")
+elif idade_doador > 69:
+    print("Impedimento: maior de 69 anos.")
+elif idade_doador == 16 or idade_doador < 18:
+    print("Restrição: requer autorização do responsável.")
+elif idade_doador == 18 or idade_doador < 60:
+    print("Sem impedimentos ou restrições.")
+elif idade_doador == 60 or idade_doador < 69:
+    print(" Restrição: não pode ser a primeira doação.")
+else:
+    print("Erro!")
